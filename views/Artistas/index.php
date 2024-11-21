4cddca212102accd723325a5ae7ccad4a976eac1
Mateus Gomes <mateuscostagomes000@gmail.com>
2024-11-21 11:03:37 -0300

    feat: add new pages

---
+++ views/Artistas/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <?php include "../includes/head.php"; ?>
+    <title>Artistas</title>
+    <link rel="stylesheet" href="../assets/css/artistas.css">
+</head>
+<body>
+    <?php include "../includes/header.php"; ?>
+    <main>
+        <h1 class="titleSection">Nossos artistas</h1>
+        <div class="container">
+            <div class="card">
+                <p>Alok</p>
+            </div>
+        </div>
+    </main>
+    <?php include "../includes/footer.php"; ?>
+
+    <?php include "../includes/scripts.php"; ?>
+</body>
+</html>
---
+++ views/Vip/index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <?php include "../includes/head.php"; ?>
+    <title>VIP Lounge</title>
+    <link rel="stylesheet" href="../assets/css/vip.css">
+</head>
+<body>
+    <?php include "../includes/header.php"; ?>
+    <main>
+        <h2 class="titleSection vipColor">VIP Experience</h2>
+        <p>Seja bem-vindo (a) Maria!</p>
+        <div class="btnContainer">
+            <a href="" class="btnRed">Renovação</a>
+            <a href="" class="btnRed">Cancelamento</a>
+        </div>
+        <div class="vipBanner">
+            <div class="slideVip">
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+            </div>
+        </div>
+        <div class="photos">
+            <div class="bar">openbar</div>
+            <div class="food">openfood</div>
+            <div class="vendas">pré-vendas</div>
+        </div>
+        <div class="vipBanner">
+            <div class="slideVip">
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+                <span>
+                    <h4>VIP</h4>
+                    <img src="../assets/img/crown-white.svg" alt="Coroa VIP branca">
+                </span>
+            </div>
+        </div>
+        
+    </main>
+    <?php include "../includes/footer.php"; ?>
+
+    <?php include "../includes/scripts.php"; ?>
+    <script src="../assets/js/home.js"></script>
+</body>
+</html>
